a3d87f53c537e7ca65d46734ca0e799e02cda957
Hector Villasano <hectorstevenvillasano@gmail.com>
2021-04-09 14:15:20 -0700

    Pipe

--- flappy_bird.py
+++ flappy_bird.py
@@ -9,7 +9,7 @@ import random
 from pygame import Surface
 from pygame.surface import SurfaceType
 
-WIND_WIDTH = 600
+WIND_WIDTH = 500
 WIN_HEIGHT = 800
 
 
@@ -29,11 +29,12 @@ PIPE_IMG = create_surface('pipe.png')
 BASE_IMG = create_surface('base.png')
 BG_IMG = create_surface('bg.png')
 
+
 class Bird:
     IMAGES = BIRD_IMGES
     MAX_ROTATION = 25
     ROT_VEL = 20
-    ANIMATIONAL_TIME = 5
+    ANIMATION_TIME = 5
 
     def __init__(self, x, y):
         self.x = x
@@ -43,3 +44,123 @@ class Bird:
         self.vel = 0
         self.height = self.y
         self.img = self.IMAGES[0]
+        self.img_count = 0
+
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
+    def move(self):
+        self.tick_count += 1
+
+        d = self.vel * self.tick_count + 1.5 * self.tick_count ** 2
+
+        if d >= 16:
+            d = 16
+
+        if d < 0:
+            d -= 2
+
+        self.y = self.y + d
+
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.ROT_VEL
+
+    def draw(self, win):
+        self.img_count += 1
+
+        if self.img_count < self.ANIMATION_TIME:
+            self.img = self.IMAGES[0]
+        elif self.img_count < self.ANIMATION_TIME * 2:
+            self.img = self.IMAGES[1]
+        elif self.img_count < self.ANIMATION_TIME * 3:
+            self.img = self.IMAGES[2]
+        elif self.img_count < self.ANIMATION_TIME * 4:
+            self.img = self.IMAGES[1]
+        elif self.img_count < self.ANIMATION_TIME * 4 + 1:
+            self.img = self.IMAGES[0]
+            self.img_count = 0
+
+        if self.tilt <= -80:
+            self.img = self.IMAGES[1]
+            self.img_count = self.ANIMATION_TIME * 2
+
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft=(self.x, self.y)).center)
+        win.blit(rotated_image, new_rect)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+
+
+class Pipe:
+    GAP = 200
+    VEL = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+
+        self.gap = 100
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTTOM = PIPE_IMG
+
+        self.passed()
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height = self.GAP
+
+    def move(self):
+        self.x -= self.VEL
+
+    def draw(self, win):
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        
+
+
+
+
+def draw_window(win, bird):
+    win.blit(BG_IMG, (0, 0))
+    bird.draw(win)
+    pygame.display.update()
+
+
+def main():
+    bird = Bird(200, 200)
+    win = pygame.display.set_mode((WIND_WIDTH, WIN_HEIGHT))
+    clok = pygame.time.Clock()
+
+    run = True
+    while run:
+        clok.tick(30)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        bird.move()
+        draw_window(win, bird)
+
+    pygame.quit()
+    quit()
+
+
+main()
